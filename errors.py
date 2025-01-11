db281350764337a2353c15bfd605bd947536a96a
Insarov Kuzma <kuzma.insarov813@gmail.com>
2025-01-11 22:40:11 +0200

    Solution2

--- errors.py
+++ errors.py
@@ -3,18 +3,12 @@ class VaccineError(Exception):
 
 
 class NotVaccinatedError(VaccineError):
-    def __init__(self) -> None:
-        self.message = "Данные о вакцинации не найдены"
-        super().__init__(self.message)
+    pass
 
 
 class OutdatedVaccineError(VaccineError):
-    def __init__(self) -> None:
-        self.message = "Вакцинация просрочена"
-        super().__init__(self.message)
+    pass
 
 
 class NotWearingMaskError(Exception):
-    def __init__(self) -> None:
-        self.message = "Отсуствует маска"
-        super().__init__(self.message)
+    pass
